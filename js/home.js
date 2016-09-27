b4a215dd07540be7f292c6498aeaf5214d0b3665
Aline Lee Nogueira and Tiago Vasconcelos <login+alinelee+tisvasconcelos@elo7.com>
2016-09-27 20:15:12 -0300

    Adiciona service worker

--- js/home.js
+++ js/home.js
@@ -11,4 +11,15 @@
 	mapa.addEventListener('mouseleave', function() {
 		mapa.classList.remove('selecionado');
 	});
+
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('sw.js', {scope: '/front7/'})
+			.then(function(reg) {
+				// registration worked
+				console.log('Registration succeeded. Scope is ' + reg.scope);
+			}).catch(function(error) {
+				// registration failed
+				console.log('Registration failed with ' + error);
+			});
+	}
 })();
