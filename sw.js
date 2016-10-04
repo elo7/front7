33ea782aa2d820445e647b87533faade144dfd7a
Elia Martell <login+martell@elo7.com>
2016-10-04 17:01:23 -0300

    Atualizando o manifesto para pwa

--- sw.js
+++ sw.js
@@ -1,53 +1,63 @@
-var CACHE_NAME = '0.2';
+var CACHE_NAME = '0.3';
 var CACHE_URLS = [
-	'/front7/',
-	'/front7/js/home.js',
-	'/front7/estilos/front7.css',
-	'/front7/estilos/reset.css',
-	'/front7/imagens/cover.jpg',
-	'/front7/palestrantes/aline.jpg',
-	'/front7/palestrantes/fernanda.jpg',
-	'/front7/palestrantes/luiz.jpg',
-	'/front7/palestrantes/sergio.jpg',
-	'/front7/palestrantes/tiago.jpg',
-	'/front7/imagens/favicons/favicon-16x16.png',
-	'/front7/imagens/favicons/favicon-32x32.png',
-	'/front7/imagens/favicons/favicon-48x48.png',
-	'/front7/imagens/favicons/favicon-57x57.png',
-	'/front7/imagens/favicons/favicon-60x60.png',
-	'/front7/imagens/favicons/favicon-64x64.png',
-	'/front7/imagens/favicons/favicon-72x72.png',
-	'/front7/imagens/favicons/favicon-76x76.png',
-	'/front7/imagens/favicons/favicon-96x96.png',
-	'/front7/imagens/favicons/favicon-114x114.png',
-	'/front7/imagens/favicons/favicon-120x120.png',
-	'/front7/imagens/favicons/favicon-128x128.png',
-	'/front7/imagens/favicons/favicon-144x144.png',
-	'/front7/imagens/favicons/favicon-152x152.png',
-	'/front7/imagens/favicons/favicon-160x160.png',
-	'/front7/imagens/favicons/favicon-180x180.png',
-	'/front7/imagens/favicons/favicon-192x192.png'
+	'index.html',
+	'js/home.js',
+	'estilos/front7.css',
+	'estilos/reset.css',
+	'imagens/cover.jpg',
+	'palestrantes/aline.jpg',
+	'palestrantes/fernanda.jpg',
+	'palestrantes/luiz.jpg',
+	'palestrantes/sergio.jpg',
+	'palestrantes/tiago.jpg',
+	'imagens/favicons/favicon-16x16.png',
+	'imagens/favicons/favicon-32x32.png',
+	'imagens/favicons/favicon-48x48.png',
+	'imagens/favicons/favicon-57x57.png',
+	'imagens/favicons/favicon-60x60.png',
+	'imagens/favicons/favicon-64x64.png',
+	'imagens/favicons/favicon-72x72.png',
+	'imagens/favicons/favicon-76x76.png',
+	'imagens/favicons/favicon-96x96.png',
+	'imagens/favicons/favicon-114x114.png',
+	'imagens/favicons/favicon-120x120.png',
+	'imagens/favicons/favicon-128x128.png',
+	'imagens/favicons/favicon-144x144.png',
+	'imagens/favicons/favicon-152x152.png',
+	'imagens/favicons/favicon-160x160.png',
+	'imagens/favicons/favicon-180x180.png',
+	'imagens/favicons/favicon-192x192.png'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('activate', function(event) {
 	event.waitUntil(
-		caches
-			.keys()
-			.then(versions => {
-				versions
-					.filter(version => version != CACHE_NAME)
-					.forEach(version => caches.delete(version));
-			}).then(() => {
-				caches.open(CACHE_NAME).then(cache => {
-					return cache.addAll(CACHE_URLS);
-				});
-			})
+		caches.keys().then(function(versions) {
+			return Promise.all(
+				versions.map(function(version) {
+					if (CACHE_NAME != version) {
+						return caches.delete(version);
+					}
+				})
+			);
+		})
+	);
+});
+
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function(cache) {
+			return cache.addAll(CACHE_URLS);
+		})
 	);
 });
 
 self.addEventListener('fetch', function(event) {
 	if(event.request.url.indexOf(self.location.origin) != -1) {
-		event.respondWith(caches.match(event.request));
+		event.respondWith(
+			caches.match(event.request).then(function(response){
+				return response || fetch(event.request.clone());
+			})
+		);
 	}
 });
 
