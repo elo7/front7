7429492ec665bc9bb179c0ed41dc0d02bdba007b
Mateus Molinaro Motta <login+mottam@elo7.com>
2016-10-10 17:01:09 -0300

    Spike de amp no front7

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let express = require('express'),
   compression = require('compression'),
   bodyParser = require('body-parser'),
   app = express();
+  fs = require('fs'),
+  ampTemplates = new Map(),
+  templatesDir = 'views/templates';
 
 app.use(compression());
 app.use(bodyParser());
@@ -16,8 +19,40 @@ app.use('/views', express.static('views'));
 app.use('/data', express.static('data'));
 
 app.engine('html', handlebars.create({defaultLayout: 'main', helpers: handlebarsHelpers, extname: 'html'}).engine);
-app.set('views', 'views/templates');
+app.set('views', templatesDir);
 app.set('view engine', 'html');
+
+app.use(function(req, res, next) {
+		let _render = res.render;
+		let ampUrl = (url) => {
+			const ampParam = '?amp=true';
+			return url.indexOf(ampParam) >= 0 ? url : url + ampParam;
+		}
+
+		res.render = function() {
+			if (typeof arguments[1] === 'object') {
+				arguments[1].canonical = req.url;
+
+				if (ampTemplates.has(arguments[0]) && ampTemplates.get(arguments[0])) {
+					arguments[1].ampurl = ampUrl(req.url);
+				} else {
+					const ampFile = fs.existsSync(`${__dirname}/${templatesDir}/${arguments[0]}-amp.html`);
+					if (ampFile) {
+						ampTemplates.set(arguments[0], true);
+						arguments[1].ampurl = ampUrl(req.url);
+
+						if (req.query.amp) {
+							arguments[0] += '-amp';
+							arguments[1].amp = true;
+						}
+					}
+				}
+
+			}
+			_render.apply(this, arguments);
+		};
+	next();
+});
 
 let homeController = require('./controllers/home'),
 	apiController = require('./controllers/api');
